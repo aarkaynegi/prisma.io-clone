47fa422bc62d1014acf042cfe1cacdd852940aaa
Rohit K <55015008+aarkaynegi@users.noreply.github.com>
2023-09-14 19:31:51 +0530

    ui: navbar updated

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,10 +5,26 @@ import './Header.css'
 
 export function Header() {
   return (
-    <div className="flex justify-between items-center max-w-[1248px] px-[1.5rem] py-[8px] m-auto">
-      <HeaderLogo />
-      <NavBar />
-      <HeaderButton />
-    </div>
+    <>
+      <div className="w-full">
+        <div className="fontInter bg-[#1a202c] w-full p-[16px]">
+          <div className="max-w-[1200px] flex justify-center items-center m-auto text-center">
+            <a
+              href="https://pris.ly/pdp"
+              className="text-[#e2e8f0] leading-[24px] font-[400] text-[18px]"
+            >
+              <b className="text-[#71e8df] font-bolder">Accelerate</b> in
+              Preview: Global database cache with scalable connection pooling
+              -&gt; <u className="underline">Get started</u>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-between items-center max-w-[1248px] px-[1.5rem] py-[8px] m-auto">
+        <HeaderLogo />
+        <NavBar />
+        <HeaderButton />
+      </div>
+    </>
   )
 }
--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -277,6 +277,122 @@ export const NavBar: React.FunctionComponent = () => {
               <a href="./company">Company</a>
               <i className="fa-solid fa-chevron-down"></i>
             </div>
+            <div className="nav-sub-item developer_nav-sub-items">
+              <div className="">
+                <div className="pt-[24px] mt-[12px] ">
+                  <div className="w-[689px] py-[2rem]  bg-white shadow-[rgb(0 0 0 / 6%)] rounded-[20px] overflow-hidden shadow-[0_4px_20px_rgba(0,0,0,0.06)]">
+                    <div className="flex border-r-1 border-[#e2e8f0]">
+                      <div className="items ">
+                        <a
+                          href=" /about"
+                          className="pointer pr-[1rem] pl-[2rem] mb-[1rem] flex fontInter gap-[12px] items-center"
+                        >
+                          <div className="p-[8px]">
+                            <i className="fa-solid fa-info"></i>
+                          </div>
+                          <div>
+                            <div className="nav-sub-item-heading font-semibold mb-[0.25rem] text-[#2d3748]">
+                              About
+                            </div>
+                          </div>
+                        </a>
+                        <a
+                          href="/blog"
+                          className="pointer pr-[1rem] pl-[2rem] mb-[1rem] flex fontInter gap-[12px] items-center"
+                        >
+                          <div className="p-[8px]">
+                            <i className="fa-regular fa-message"></i>
+                          </div>
+                          <div>
+                            <div className="nav-sub-item-heading font-semibold mb-[0.25rem] text-[#2d3748]">
+                              Blog
+                            </div>
+                          </div>
+                        </a>
+                        <a
+                          href="/careers"
+                          className="pointer pr-[1rem] pl-[2rem] mb-[1rem] flex fontInter gap-[12px] items-center"
+                        >
+                          <div className="p-[8px]">
+                            <i className="fa-solid fa-chart-line"></i>
+                          </div>
+                          <div>
+                            <div className="nav-sub-item-heading font-semibold mb-[0.25rem] text-[#2d3748]">
+                              Careers
+                            </div>
+                          </div>
+                        </a>
+                        <a
+                          href="/events"
+                          className="pointer pr-[1rem] pl-[2rem] mb-[1rem] flex fontInter gap-[12px] items-center"
+                        >
+                          <div className="p-[8px]">
+                            <i className="fa-solid fa-users-viewfinder"></i>
+                          </div>
+                          <div>
+                            <div className="nav-sub-item-heading font-semibold mb-[0.25rem] text-[#2d3748]">
+                              Events
+                            </div>
+                          </div>
+                        </a>
+                        <a
+                          href="https://pris.ly/causes"
+                          className="pointer pr-[1rem] pl-[2rem] mb-[1rem] flex fontInter gap-[12px] items-center"
+                        >
+                          <div className="p-[8px]">
+                            <i className="fa-solid fa-hand-holding-heart"></i>
+                          </div>
+                          <div>
+                            <div className="nav-sub-item-heading flex items-center gap-[4px] font-semibold mb-[0.25rem] text-[#2d3748]">
+                              Causes
+                              <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                            </div>
+                          </div>
+                        </a>
+                      </div>
+                      <div className="block w-[1px] bg-[#e2e8f0] my-[0.75rem]"></div>
+                      <div className="articles px-[24px]">
+                        <h4 className="!mt-[12px] !mb-[32px] fontBarlow !font-[500] tracking-[0.1em] leading-1 uppercase text-[#718096] !p-0">
+                          Latest from the blog
+                        </h4>
+                        <a
+                          href="https://www.prisma.io/blog/accelerate-preview-release-ab229e69ed2"
+                          className="flex gap-[18px] mb-[32px] items-center bg-transparent"
+                        >
+                          <div className="article-image max-w-[148px] w-full">
+                            <img
+                              src="images/nav-article/nav-article-1.svg"
+                              alt="Accelerate in Preview: Global Database Cache &amp; Scalable Connection Pool"
+                              className="rounded-[4px] w-full"
+                            />
+                          </div>
+                          <p className="font-[500] fontInter text-[1rem] leading-[1.3] text-[#718096]">
+                            Accelerate in Preview: Global Database Cache &amp;
+                            Scalable Connection Pool
+                          </p>
+                        </a>
+                        <a
+                          href="https://www.prisma.io/blog/cloudflare-partnership-qerefgvwirjq"
+                          className="flex gap-[18px] mb-[32px] items-center bg-transparent"
+                        >
+                          <div className="article-image max-w-[148px] w-full">
+                            <img
+                              src="images/nav-article/nav-article-2.svg"
+                              alt="Developer Experience Redefined: Prisma &amp; Cloudflare Lead the Way to Data DX"
+                              className="rounded-[4px] w-full"
+                            />
+                          </div>
+                          <p className="font-[500] fontInter text-[1rem] leading-[1.3] text-[#718096]">
+                            Developer Experience Redefined: Prisma &amp;
+                            Cloudflare Lead the Way to Data DX
+                          </p>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </li>
         </ul>
       </nav>
